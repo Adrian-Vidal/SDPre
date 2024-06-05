b28201a06b51aea6a7f42b17bbf406ba54991eee
Adrian <adrian.vidal9589@gmail.com>
2024-06-05 11:02:32 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Adrian Vidal";
+    int age = 34;
+    String state = "Colorado";
+
+    
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+    
+    
+  }
+}
