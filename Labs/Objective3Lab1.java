0ee24c92037e634a9df00ec92d440032d6eda358
Adrian <adrian.vidal9589@gmail.com>
2024-06-05 14:33:58 -0600

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,17 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    
+    double num1 = 19;
+    double num2 = 22;
+    double num3 = 36;
+    double average = (num1 + num2 + num3) / 3;
+
+    System.out.println(average);
+    
+
+    
+
+
+
+  }
+}
