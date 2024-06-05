01ebd241b051e6d75ee402f0175f815cb892e120
Adrian <adrian.vidal9589@gmail.com>
2024-06-05 16:04:00 -0600

    complete objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,18 @@
+public class Objective3Lab2 {
+	public static void main(String[] args) {
+
+		double side1 = 5;
+		double side2 = 11;
+		double side1squared = (side1 * side1);
+		double side2squared = (side2 * side2);
+		double hypotenuse = Math.sqrt(side1squared + side2squared);
+
+		System.out.println("The hypotenuse of a triangle with sides " + side1 + " and " + side2 + " is " + hypotenuse);
+
+
+
+
+
+
+	}
+}
