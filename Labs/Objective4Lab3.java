7339c922b00d586e42e80a88cd94dfd417bec8b7
Adrian <adrian.vidal9589@gmail.com>
2024-06-06 14:59:07 -0600

    complete objective4lab3

--- Labs/Objective4Lab3.java
+++ Labs/Objective4Lab3.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class Objective4Lab3 {
   public static void main(String[] args) {
-    int birthYear, currentYear;
+    int currentYear;
 
-    birthYear = 0;
     currentYear = 2024;
     
 
