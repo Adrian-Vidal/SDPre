10474f09bf079592916f587c4ec7889b4227e942
Adrian <adrian.vidal9589@gmail.com>
2024-06-06 09:50:50 -0600

    complete objective4lab3

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Objective4Lab3 {
+  public static void main(String[] args) {
+    int birthYear, currentYear;
+
+    birthYear = 0;
+    currentYear = 2024;
+    
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("How old are you?");
+    int age = input.nextInt();
+
+
+    System.out.println("You were born in " + (currentYear - age));
+    input.close();
+  }
+}
